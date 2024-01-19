87946db909212be389dd2211e45a2d7dd029bc98
KyawZinHtike1304 <kyawzinhtike1304@gmail.com>
2024-01-19 09:17:51 +0630

    firstDay

---
+++ src/main/java/com/example/securitymasterdemo/security/SecurityRoles.java
@@ -0,0 +1,9 @@
+package com.example.securitymasterdemo.security;
+
+public interface SecurityRoles {
+
+    String ROLE_PREFIX = "ROLE_";
+    String ROLE_ADMIN ="SUPER_ADMIN";
+    String DEPARTMENT_ADMIN = "DEPARTMENTS_ADMIN";
+    String DEPARTMENT_READ = "DEPARTMENTS_READ";
+}
