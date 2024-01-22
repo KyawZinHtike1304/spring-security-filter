167a5cf0dbb3e7c54aaa9640328ceb99ef7c5455
KyawZinHtike1304 <kyawzinhtike1304@gmail.com>
2024-01-22 23:33:40 +0630

    last commit

--- src/main/java/com/example/securitymasterdemo/controller/GlobalErrorController.java
+++ src/main/java/com/example/securitymasterdemo/controller/GlobalErrorController.java
@@ -1,21 +1,33 @@
 package com.example.securitymasterdemo.controller;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.HttpClientErrorException;
 
-@ControllerAdvice
-@Order(Ordered.HIGHEST_PRECEDENCE)
-public class GlobalErrorController {
+@Controller
+public class GlobalErrorController implements ErrorController{
 
-    @ExceptionHandler({Exception.class})
-    public String handleException(HttpClientErrorException.Forbidden ex, Model model){
-        System.out.println("Hello");
-        System.out.println(ex.getClass().getSimpleName());
-        model.addAttribute("message",ex.getMessage());
-        return "error";
-    }
+   @GetMapping("/error")
+   public String handleRequest(HttpServletRequest request, Model model){
+       Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+       Object message = request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
+
+       if(status != null){
+           int statusCode = Integer.parseInt(status.toString());
+           model.addAttribute("statusCode",statusCode);
+           model.addAttribute("message",message);
+       }else {
+           model.addAttribute("statusCode","Not Identified");
+           model.addAttribute("message","No message");
+       }
+       return "error";
+   }
 }
